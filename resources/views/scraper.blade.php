3c6f4dad455f8abd1a1b3771fd1a887af8c8b0ed
finegreat <c1027120@my.shu.ac.uk>
2023-02-17 01:34:55 +0000

    nav (unimplemented)

--- resources/views/scraper.blade.php
+++ resources/views/scraper.blade.php
@@ -5,36 +5,48 @@
         </h1>
     </x-slot>
 
-<html>
-    <head>
-        <title>Scraped URLs</title>
-    </head>
-    <body>
-            @if (empty($urls['facebookUrls']) && empty($urls['twitterUrls']) && empty($urls['instaUrls']))
-        <p>No social media links found.</p>
-    @else
-        <h4>Facebook URLs:</h4>
-        <ul>
-            @foreach ($urls['facebookUrls'] as $url)
-                <li><a href="{{ $url }}">{{ $url }}</a></li>
-            @endforeach
-        </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        @if (empty($urls['facebookUrls']) && empty($urls['twitterUrls']) && empty($urls['instaUrls']))
+                            <p>No social media links found.</p>
+                        @else
+                            <h4>Facebook URLs:</h4>
+                            <ul>
+                                @foreach ($urls['facebookUrls'] as $url)
+                                    <li><a href="{{ $url }}">{{ $url }}</a></li>
+                                @endforeach
+                            </ul>
 
-        <h4>Twitter URLs:</h4>
-        <ul>
-            @foreach ($urls['twitterUrls'] as $url)
-                <li><a href="{{ $url }}">{{ $url }}</a></li>
-            @endforeach
-        </ul>
+                            <h4>Twitter URLs:</h4>
+                            <ul>
+                                @foreach ($urls['twitterUrls'] as $url)
+                                    <li><a href="{{ $url }}">{{ $url }}</a></li>
+                                @endforeach
+                            </ul>
 
-        <h4>Instagram URLs:</h4>
-        <ul>
-            @foreach ($urls['instaUrls'] as $url)
-                <li><a href="{{ $url }}">{{ $url }}</a></li>
-            @endforeach
-        </ul>
-        @endif
-    </body>
-</html>
+                            <h4>Instagram URLs:</h4>
+                            <ul>
+                                @foreach ($urls['instaUrls'] as $url)
+                                    <li><a href="{{ $url }}">{{ $url }}</a></li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                 </ul>
+            </nav>
+        </div>
+    </div>
 
-</x-app-layout>
+</x-guest-layout>
